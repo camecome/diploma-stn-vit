c46c90dbf153306b27d4cabe24a2c3992ee2535c
Dmitrii Krivogub <dkrivogub@10.215.159.243-vpn.dhcp.yndx.net>
2026-05-28 22:01:37 +0300

    fixed basic data utils

--- diploma_stn_vit/utils/data_utils.py
+++ diploma_stn_vit/utils/data_utils.py
@@ -1,12 +1,12 @@
 import logging
 
+import math
 import torch
 
 from torchvision import transforms, datasets
 from torch.utils.data import (
     DataLoader,
     RandomSampler,
-    DistributedSampler,
     SequentialSampler,
 )
 
@@ -15,38 +15,56 @@ from pathlib import Path
 logger = logging.getLogger(__name__)
 
 
-def get_loader(args):
-    if args.local_rank not in [-1, 0]:
-        torch.distributed.barrier()
+def get_safe_rotation_size(img_size, max_rotation_degrees):
+    angle = math.radians(abs(max_rotation_degrees))
+    return math.ceil(img_size * (math.cos(angle) + math.sin(angle)))
 
-    transform_train = transforms.Compose(
-        [
-            transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        ]
-    )
-    transform_test = transforms.Compose(
-        [
-            transforms.Resize((args.img_size, args.img_size)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        ]
-    )
+
+def get_loader(args):
+    if args.max_rotation_degrees is not None:
+        safe_size = get_safe_rotation_size(args.img_size, args.max_rotation_degrees)
+        transform_train = transforms.Compose(
+            [
+                transforms.Resize((safe_size, safe_size)),
+                transforms.RandomRotation(degrees=args.max_rotation_degrees),
+                transforms.CenterCrop((args.img_size, args.img_size)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ]
+        )
+        transform_test = transforms.Compose(
+            [
+                transforms.Resize((safe_size, safe_size)),
+                transforms.RandomRotation(degrees=args.max_rotation_degrees),
+                transforms.CenterCrop((args.img_size, args.img_size)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ]
+        )
+    else:
+        transform_train = transforms.Compose(
+            [
+                transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ]
+        )
+        transform_test = transforms.Compose(
+            [
+                transforms.Resize((args.img_size, args.img_size)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ]
+        )
 
     data_dir = Path(args.dataset_path)
 
     trainset = datasets.ImageFolder(root=data_dir / "train", transform=transform_train)
-    testset = (
-        datasets.ImageFolder(root=data_dir / "val", transform=transform_test) if args.local_rank in [-1, 0] else None
-    )
+    testset = datasets.ImageFolder(root=data_dir / "val", transform=transform_test)
 
-    if args.local_rank == 0:
-        torch.distributed.barrier()
+    train_sampler = RandomSampler(trainset) # shuffle indexes
+    test_sampler = SequentialSampler(testset) # get indexes one by one w/o shuffle
 
-    train_sampler = RandomSampler(trainset) if args.local_rank == -1 else DistributedSampler(trainset)
-
-    test_sampler = SequentialSampler(testset)
     train_loader = DataLoader(
         trainset,
         sampler=train_sampler,
@@ -62,8 +80,6 @@ def get_loader(args):
             num_workers=8,
             pin_memory=True,
         )
-        if testset is not None
-        else None
     )
 
     return train_loader, test_loader
